07f38b57598095cc9222a2ab4020eb082122cbea
nikos <nikgiannsm@gmail.com>
2024-10-29 18:21:28 +0200

    Added a get request.

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from flask import Flask
 from flask_restful import Api, Resource
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 api = Api(app)
 
+class HelloWorld(Resource):
+    def get(self):
+        return {"data": "Hello World."}
+
+api.add_resource(HelloWorld, "/")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("FLASK_PORT" , 5000))
+    app.run(debug=True, port=port)
