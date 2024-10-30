d8a95d323181974d8e480cfc627c1d9f39994403
nikos <nikgiannsm@gmail.com>
2024-10-30 16:03:56 +0200

    Added GET by id and POST video endpoints

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 import os
 from dotenv import load_dotenv
@@ -9,6 +9,7 @@ load_dotenv()
 app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class VideoModel(db.Model):
@@ -22,31 +23,31 @@ video_put_args.add_argument("name" , type=str , help="Name of video is required"
 video_put_args.add_argument("views" , type=int , help="Views of video is required" , required=True)
 video_put_args.add_argument("likes" , type=int , help="Likes of video is required" , required=True)
 
-videos = {}
-
-def abort_if_not_valid_id(video_id):
-    if video_id not in videos:
-        abort(404 , message="Video id is not valid.")
-
-def abort_if_video_already_exists(video_id):
-    if video_id in videos:
-        abort(404 , message="Video already exist.")
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer
+}
 
 class Video(Resource):
+    @marshal_with(resource_fields)
     def get(self, video_id):
-        abort_if_not_valid_id(video_id)
-        return videos[video_id]
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Video id could not be found.")
+        return result
     
+    @marshal_with(resource_fields)
     def post(self, video_id):
-        abort_if_video_already_exists
         args = video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201
-    
-    def delete(self, video_id):
-        abort_if_not_valid_id
-        del videos[video_id]
-        return "Video deleted" , 204
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(404, message="Video id already exist.")
+        video = VideoModel(id=video_id, name=args['name'] , views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
     
 api.add_resource(Video, "/video/<int:video_id>")
 
