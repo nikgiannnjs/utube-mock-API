e0aec7e19b88335f557d28b5005aae1827e5c804
nikos <nikgiannsm@gmail.com>
2024-10-30 16:24:20 +0200

    Added UPDATE endpoint

--- main.py
+++ main.py
@@ -23,6 +23,11 @@ video_put_args.add_argument("name" , type=str , help="Name of video is required"
 video_put_args.add_argument("views" , type=int , help="Views of video is required" , required=True)
 video_put_args.add_argument("likes" , type=int , help="Likes of video is required" , required=True)
 
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name" , type=str , help="Name of video is required")
+video_update_args.add_argument("views" , type=int , help="Views of video is required")
+video_update_args.add_argument("likes" , type=int , help="Likes of video is required")
+
 resource_fields = {
     'id': fields.Integer,
     'name': fields.String,
@@ -33,22 +38,48 @@ resource_fields = {
 class Video(Resource):
     @marshal_with(resource_fields)
     def get(self, video_id):
+        
         result = VideoModel.query.filter_by(id=video_id).first()
+        
         if not result:
             abort(404, message="Video id could not be found.")
+        
         return result
     
     @marshal_with(resource_fields)
     def post(self, video_id):
         args = video_put_args.parse_args()
+        
         result = VideoModel.query.filter_by(id=video_id).first()
         if result:
             abort(404, message="Video id already exist.")
+        
         video = VideoModel(id=video_id, name=args['name'] , views=args['views'], likes=args['likes'])
+       
         db.session.add(video)
         db.session.commit()
+        
         return video, 201
     
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Video id could not be found. Cannot update.")
+        
+        if args['name']:
+            result.name = args['name']
+        if args['views']:
+            result.name = args['views']
+        if args['likes']:
+            result.likes = args['likes']
+        
+        db.session.commit()
+        
+        return result
+
 api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
